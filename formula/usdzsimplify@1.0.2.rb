99dc15b1a9acfc68c69b356040a28c4e0793e0c5
J <1107882701@qq.com>
2025-12-26 15:46:28 +0800

    1.0.2

---
+++ formula/usdzsimplify@1.0.2.rb
@@ -0,0 +1,29 @@
+class UsdzsimplifyAT102 < Formula
+  desc "A Swift-powered tool for simplifying USDZ models"
+  homepage "https://github.com/cqj1558/USDZSimplifySPM"
+  url "https://github.com/cqj1558/USDZSimplifySPM/archive/refs/tags/1.0.2.tar.gz"
+  sha256 "42e355471031eb7e220c788500595fb28080d0a9f2f0a64632037ec460bbdeef"
+  license "MIT"
+
+  # 声明 macOS 版本要求（根据 Package.swift 中的 platforms）
+  depends_on :macos => :sequoia  # macOS 15.0 (Sequoia)
+  depends_on "swift" => :build
+
+  def install
+    # 使用 swift build 命令编译项目
+    system "swift", "build", "-c", "release", "--disable-sandbox", "--product", "usdzutil"
+    
+    # 将编译好的二进制文件安装到 Homebrew 的 bin 目录
+    bin.install ".build/release/usdzutil"
+  end
+
+  test do
+    # 测试工具是否能正常运行（使用 --help 而不是 --version）
+    # 因为 ArgumentParser 默认支持 --help
+    assert_match "USDZ 模型简化工具", shell_output("#{bin}/usdzutil --help")
+    
+    # 或者测试 simplify 子命令的帮助信息
+    assert_match "简化单个 USDZ 文件", shell_output("#{bin}/usdzutil simplify --help")
+  end
+end
+
